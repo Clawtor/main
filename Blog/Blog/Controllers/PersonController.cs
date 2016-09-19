e30ebe2ea7b97bd45f62aeb24ca85191444a26fb
Tim Watson <tim.watson.nz@gmail.com>
2016-09-19 21:27:15 +1200

    Committing project

---
+++ Blog/Blog/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Blog.Controllers
+{
+	public class PersonController : ApiController
+	{
+		public IHttpActionResult Get()
+		{
+			return Ok("Hello, web api");
+		}
+	}
+}
